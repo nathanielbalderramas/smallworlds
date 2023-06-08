78d194e5a6e62bdf46ec76db8c35772467879bda
nathanielbalderramas <nathanielbalderramas@gmail.com>
2023-06-08 08:07:22 -0300

    implements reproduce, is_satisfied, is_succesfull_attempt, produce_offspring methods on organism. also refactors __str__.

--- organism.py
+++ organism.py
@@ -1,6 +1,5 @@
 import textwrap
-
-import setuptools
+import random
 
 
 class Organism:
@@ -11,6 +10,10 @@ class Organism:
         new_id = Organism.last_id
         Organism.last_id += 1
         return new_id
+
+    @staticmethod
+    def is_successful_attempt(reference):
+        return random.randint(0, 100) <= reference
 
     def __init__(
         self,
@@ -50,7 +53,7 @@ class Organism:
         self.reproduced_this_epoch = False
         self.offspring = []
 
-    def play_routine(self):
+    def advance_time(self):
         self.move()
         self.eat()
         self.reproduce()
@@ -64,15 +67,36 @@ class Organism:
         pass
 
     def reproduce(self):
-        pass
+        if self.is_satiated() and self.is_successful_attempt(self.offspring_chance):
+            self.produce_offspring()
 
     def __str__(self):
+        # self string formatting
         txt = f"{self.species_name} {self.id}"
         txt += f"\nposition: {self.position} | velocity: {self.velocity} | hunger: {self.hunger}"
+
+        # offspring string formatting
         if len(self.offspring):
+            offspring_strings = ["\n" + str(offspring) for offspring in self.offspring]
             txt += f"\nOffspring:\n" + 10 * "-"
-            txt += textwrap.indent(
-                "".join(["\n" + str(offspring) for offspring in self.offspring]), "    "
-            )
+            txt += textwrap.indent("".join(offspring_strings), "\t")
             txt += "\n" + 10 * "-"
+
         return txt
+
+    def is_satiated(self):
+        return self.hunger <= 0
+
+    def produce_offspring(self):
+        for i in range(self.litter_size):
+            child = Organism(
+                self.species_name,
+                self.navigator,
+                self.speed,
+                self.base_hunger,
+                self.feeding_range,
+                self.feeding_chance,
+                self.offspring_chance,
+                self.litter_size,
+            )
+            self.offspring.append(child)
