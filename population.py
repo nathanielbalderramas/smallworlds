623bb8bc8cbb088956920624b4cf256ca91c2c68
nathanielbalderramas <83505517+nathanielbalderramas@users.noreply.github.com>
2023-06-09 15:27:06 -0300

    improves documentation on population.py

--- population.py
+++ population.py
@@ -2,28 +2,44 @@ from organism import Organism
 
 
 class Population:
+    """
+    This is mainly a collection of Organisms of the same species.
+    It has the ability to create more organisms, add already created
+    offspring organisms and remove dead ones.
+
+    non obvious attributes:
+    -----------------------
+    individuals_specs: is a dictionary containing arguments for initialization of organisms
+    """
     def __init__(
         self,
-        species_name: str,
-        species_trophic_level: int,
-        initial_size: int,
         individuals_specs: dict,
+        initial_size: int,
     ):
-        self.species_name = species_name
-        self.species_trophic_level = species_trophic_level
+        self.species_name = individuals_specs["species_name"]
+        self.trophic_level = individuals_specs["trophic_level"]
         self.individuals_specs = individuals_specs
         self.living_individuals = []
         self.dead_individuals = []
         for i in range(initial_size):
             self.create_individual()
 
-    def add_individual(self, organism: Organism):
+    def add_individual(self, organism: Organism) -> None:
+        """
+        adds an already existing individual (mainly used for offspring instances)
+        :param organism: Organism
+        :return:
+        """
         self.living_individuals.append(organism)
 
-    def create_individual(self):
-        self.living_individuals.append(Organism(*self.individuals_specs))
+    def create_individual(self) -> None:
+        """
+        creates a new individual calling on the Organism constructor with individuals_specs as keyword arguments.
+        :return:
+        """
+        self.living_individuals.append(Organism(**self.individuals_specs))
 
-    def remove_individual(self, individual: Organism):
+    def remove_individual(self, individual: Organism) -> None:
         """
         stashes an individual into the dead_individuals list to save its offspring for next epoch
         :param individual: Organism
@@ -32,9 +48,16 @@ class Population:
         self.dead_individuals.append(individual)
         self.living_individuals.remove(individual)
 
-    def advance_time(self):
+    def advance_time(self) -> None:
+        """
+        makes every organism in the population to run for a timestep
+        :return:
+        """
         for individual in self.living_individuals:
             individual.advance_time()
+
+    def get_living_individuals(self):
+        return self.living_individuals
 
     def advance_epoch(self):
         # generates new individuals based on the offspring of both dead and living individuals
